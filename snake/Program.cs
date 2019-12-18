651cac7252cae13269ca2c07628b2859bcd9a34f
Евгений Касаткин <wildfan@yandex.ru>
2019-12-19 00:36:22 +0300

    Create Draw function

--- snake/Program.cs
+++ snake/Program.cs
@@ -6,7 +6,17 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Draw(5, 6, '*');
+            Draw(5, 7, '*');
+            Draw(5, 8, '*');
+
+            Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
